fd867a3cec3ae89add49867a7e723ff95ad49383
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2021-09-26 14:16:06 -0300

    Update header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import { ReactComponent as Logo } from '../../Assets/usuario.svg';
+import { ReactComponent as Logo } from '../../Assets/logo_white.svg';
 
 function Header() {
 
   return (
     <header className={styles.header}>
-      <nav className={`container ${styles.nav}`}>
-        <Link to="/" aria-label="Conecta Café - Home">
-          <Logo />
-        </Link>
-        <Link to="/fazendas">
-          Fazendas
-        </Link>
-        <Link to="/login">
-          Login
-        </Link>
-        <Link to="/cadastro">
-          Cadastrar
-        </Link>
+      <nav className={`${styles.nav}`}>
+        <Logo />
+        <div className={styles.container}>
+          <Link to="/">
+            Home
+          </Link>
+          <Link to="/fazendas">
+            Fazendas
+          </Link>
+          <div className={styles.auth}>
+            <Link to="/login">
+              Entrar
+            </Link>
+            <Link to="/cadastro">
+              Cadastrar
+            </Link>
+          </div>
+        </div>
       </nav>
     </header>
   );
