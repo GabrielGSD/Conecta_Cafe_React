747694046401d27bc40c2831a224da75daa80293
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2021-09-27 21:48:55 -0300

    api google maps - obs: warnings no console

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Maps from '../Maps/Maps';
 
 function Home() {
   return (
+    <>
+      <div>
+        <h1>HOME PAGE</h1>
+      </div>
     <div>
-      <h1>HOME PAGE</h1>
+      <Maps />
     </div>
+    </>
   );
 }
 
