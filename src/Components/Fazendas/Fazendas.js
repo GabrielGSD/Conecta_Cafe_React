9fe4e00904491a4fbd1847146a9efa28a55870c1
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2021-10-16 12:42:31 -0300

    Update card and add grid

--- src/Components/Fazendas/Fazendas.js
+++ src/Components/Fazendas/Fazendas.js
@@ -1,16 +1,18 @@
 import {React, useState} from 'react'
 import styles from './Fazenda.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Form} from "react-bootstrap";
+// import {Form} from "react-bootstrap";
 import Select from '../Select/Select';
 import imgFazenda from '../../Assets/fazendaT.jpg'
+import {ReactComponent as ArrowIcon} from '../../Assets/Arrow.svg'
 
 function Fazendas() {
   const [torra, setTorra] = useState("");
   const torras = ['Clara', 'Média', 'Escura'];
   const [processo, setProcesso] = useState("");
   const processos = ['Terreiro', 'Estufa', 'Terreiro suspenso', 'Secagem automática'];
-  const [altitude, setAltitude] = useState();
+  const teste = ['A', 'S', 'D', 'E', 'F', 'G', 'H', 'J', 'K'];
+  // const [altitude, setAltitude] = useState();
   const photo = {src: imgFazenda, title:"Teste"}
 
   return (
@@ -24,12 +26,17 @@ function Fazendas() {
           <Select type={processo} setType={setProcesso} options={processos} def="Processamento" />
           {/* <input type="range" class="form-range"  max='5000' value={altitude} onChange={altitude => setAltitude(altitude)}/> */}
         </div>
-        <div className={styles.card}>
-          <img src={photo.src} alt={photo.title} />
-          <div className={styles.info}>
-            <h1 className={styles.nome}>Fazenda Paraiso</h1>
-            <h2 className={styles.regiao}>Sul de Minas - Ouro Fino</h2>
-          </div>
+        <div className="container-scroll">
+          {teste.map((card) => (
+            <div className={styles.card} key={card}>
+              <img src={photo.src} alt={photo.title} />
+              <div className={styles.info}>
+                <h1 className={styles.nome}>Fazenda Paraiso</h1>
+                <h2 className={styles.regiao}>Sul de Minas - Ouro Fino</h2>
+                <h2 className={`${styles.regiao} ${styles.animeTop}`}>Ver mais <ArrowIcon /></h2>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
