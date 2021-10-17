9077f7ba9815430237ef0ea46fb3690646460289
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2021-10-17 15:17:36 -0300

    Add Cadastro page

--- src/Components/Accounts/Cadastro/Cadastro.js
+++ src/Components/Accounts/Cadastro/Cadastro.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
+import styles from '../Accounts.module.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Form} from "react-bootstrap";
+import Input from '../../Form/Input/Input';
+import Button from '../../Form/Button/Button';
+import Error from '../../Helper/Error';
+import useForm from '../../../Hooks/useForm';
+import { UserContext } from '../../../Context/UserContext';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../../Assets/logo_white.svg';
 
 function Cadastro() {
+
+  const name = useForm();
+  const email = useForm("email");
+  const senha = useForm("password");
+  
+  const loading = false;
+
+  // const { userLogin, error, loading } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (email.validate() && senha.validate()) {
+      console.log(email.value, senha.value)
+      // userLogin(email.value, senha.value);
+    }
+  }
+
   return (
-    <div>
-      <h1>CADASTRO PAGE</h1>
+    <div className="bgBlack">
+      <div className={styles.teste}>
+        <div className="center" style={{padding: '50px'}}>
+          <Logo className={styles.logo} />
+        </div>
+
+        <div className={styles.box}>
+          <div className="center">
+            <h1 className="title">Cadastro</h1>
+          </div>
+          <Form className={styles.form} onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Input label="Nome" type="text" name="name" {...name} />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Input label="Email" type="text" name="email" {...email} />
+            </Form.Group>
+
+            <Form.Group className="mb-4" controlId="formBasicPassword">
+              <Input label="Senha" type="password" name="senha" {...senha} />
+            </Form.Group>
+            {loading ? (
+              <Button disabled>Carregando...</Button>
+            ) : (
+              <Button>Entrar</Button>
+            )}
+            {/* <Error error={error} /> */}
+          </Form>
+          <p className="mt-4 mb-5" >Já possui uma conta? 
+            <Link to="/login" style={{marginLeft: '5px'}}>
+              Entrar
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cadastro
