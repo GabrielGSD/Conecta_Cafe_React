7ddb98ff46e7ec786ba6a31df64a3701b59d1cdd
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2021-10-17 11:08:49 -0300

    trecho esquecido de comentar

--- src/Components/Fazendas/Fazendas.js
+++ src/Components/Fazendas/Fazendas.js
@@ -1,9 +1,9 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState } from 'react'
 import styles from './Fazenda.module.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 // import {Form} from "react-bootstrap";
 import Select from '../Select/Select';
-import API from '../../Services/Api.js';
+// import API from '../../Services/Api.js';
 import imgFazenda from '../../Assets/fazendaT.jpg'
 import { ReactComponent as ArrowIcon } from '../../Assets/Arrow.svg'
 
@@ -17,16 +17,16 @@ function Fazendas() {
   const photo = { src: imgFazenda, title: "Teste" }
 
 
-  const [user, setUser] = useState();
+  // const [user, setUser] = useState();
 
-  useEffect(() => {
-    API
-      .get("/farm/c43ecfab-69f0-46bd-aa38-4af3796350cf")
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   API
+  //     .get("/farm/c43ecfab-69f0-46bd-aa38-4af3796350cf")
+  //     .then((response) => setUser(response.data))
+  //     .catch((err) => {
+  //       console.error("ops! ocorreu um erro" + err);
+  //     });
+  // }, []);
 
   return (
     <div className={`bgGray center ${styles.header}`}>
@@ -44,12 +44,12 @@ function Fazendas() {
             <div className={styles.card} key={card}>
               <img src={photo.src} alt={photo.title} />
               <div className={styles.info}>
-                {/* <h1 className={styles.nome}>Fazenda Paraiso</h1>
+                <h1 className={styles.nome}>Fazenda Paraiso</h1>
                 <h2 className={styles.regiao}>Sul de Minas - Ouro Fino</h2>
-                <h2 className={`${styles.regiao} ${styles.animeTop}`}>Ver mais <ArrowIcon /></h2> */}
-                <h1 className={styles.nome}> {user?.data.farm_name}</h1>
-                <h2 className={styles.regiao}>{user?.data.address.city}</h2>
                 <h2 className={`${styles.regiao} ${styles.animeTop}`}>Ver mais <ArrowIcon /></h2>
+                {/* <h1 className={styles.nome}> {user?.data.farm_name}</h1>
+                <h2 className={styles.regiao}>{user?.data.address.city}</h2>
+                <h2 className={`${styles.regiao} ${styles.animeTop}`}>Ver mais <ArrowIcon /></h2> */}
               </div>
             </div>
           ))}
