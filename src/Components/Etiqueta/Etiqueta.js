7836628b3f81f9026c312f46446bb8efee8ca46f
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2021-10-18 21:05:19 -0300

    Etiqueta em andamento

--- src/Components/Etiqueta/Etiqueta.js
+++ src/Components/Etiqueta/Etiqueta.js
@@ -1,12 +1,10 @@
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import styles from './Etiqueta.module.css';
-// import logo from '../../Assets/logo_white.svg'
 import API from '../../Services/Api.js';
-import RequestQrCode from "../../Services/RequestQrCode";
+import RequestQrCode from "./GeneratedQrCode";
+import { ReactComponent as Logo } from '../../Assets/logo_black.svg';
 
-
-// const photo = { src: logo, title: "Teste" }
 const dataFarm = props => {
 
     API.get("/farm/c43ecfab-69f0-46bd-aa38-4af3796350cf")
@@ -26,70 +24,185 @@ const dataFarm = props => {
         });
 }
 
-
 class ComponentToPrint extends React.Component {
-    
+
     render() {
         dataFarm()
         return (
-            <div className={styles.container}>
-                <table className={styles.table}>
-                    <tr className={styles.tableBorder}>
-                        <th>
-                            <div>
-                            <img className={styles.a} />
-                            </div>
-                            <br />
-                            <div className={styles.regionCoffee}>
-                                {localStorage.getItem('farm_name')}
-                                <br />
-                                {localStorage.getItem('farm_city')} - {localStorage.getItem('farm_uf')}
-                            </div>
-                        </th>
-                        <th className={styles.tableBorder}>
-                            <div className={styles.typeCoffee}>
-                                <ul>
-                                    Variedade: {localStorage.getItem('farm_variety')}
-                                    <br />
-                                    Secagem: {localStorage.getItem('farm_process')}
-                                    <br />
-                                    Altitude: {localStorage.getItem('farm_altitude')}
-                                    <br />
-                                    Safra: {localStorage.getItem('farm_harvest')}
-                                </ul>
-                            </div>
-                            <div>
-                                {
-                                    localStorage.getItem('farm_special') ?
-                                    <div className={styles.specialCoffee}>
-                                            Corpo:
-                                            <br />
-                                            Doçura:
-                                            <br />
-                                            Acidez:
-                                            <br />
-                                        </div>
-                                        :
-                                        null
-                                    }
-                            </div>
-                        </th>
-                        <th className={styles.tableBorder}>
-                            {/* <div className={styles.contact}>
-                                Telefone: {localStorage.getItem('farm_contact')}
-                                <br />
-                                Email: {localStorage.getItem('farm_contact_email')}
-                            </div> */}
+            <div>
+                <div className={styles.container}>
+                    <table className={styles.table}>
+                        <table className={styles.table1}>
+                            <tr>
+                                <th className={styles.larguraMaior}>
+                                    <div className={styles.regionCoffee}>
+                                        {localStorage.getItem('farm_name')}
+                                        <br />
+                                        {localStorage.getItem('farm_city')} - {localStorage.getItem('farm_uf')}
+                                    </div>
+                                </th>
+                                <th >
+                                    <div className={styles.produtor}>
+                                        Produtor:
+                                        <br />
+                                        Gabriel de Souza Daniel
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table2}>
+                            <tr>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Espécie:
+                                        <br />
+                                        100% arábica
+                                    </div>
+                                </th>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Variedade:
+                                        <br />
+                                        Catuaí vermelho
+                                    </div>
+                                </th>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Altitude:
+                                        <br />
+                                        1200m
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table2}>
+                            <tr>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Processo:
+                                        <br />
+                                        Natural
+                                    </div>
+                                </th>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Safra:
+                                        <br />
+                                        2021
+                                    </div>
+                                </th>
+                                <th className={styles.larguraMedia}>
+                                    <div>
+                                        Valor da Safra:
+                                        <br />
+                                        R$ 1.500,00
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table3}>
+                            <tr>
+                                <th className={styles.larguraMenor1}>
+                                    <div >
+                                        Aroma:
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Intenso de frutas maduras
 
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table3}>
+                            <tr>
+                                <th className={styles.larguraMenor1}>
+                                    <div >
+                                        Sabor:
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Jaca, Banana, Cajá - manga e acidez de maracujá
+
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table3}>
+                            <tr>
+                                <th className={styles.larguraMenor1}>
+                                    <div >
+                                        Finalização:
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Longa, picante e muito doce
+
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table3}>
+                            <tr>
+                                <th className={styles.larguraMenor1}>
+                                    <div >
+                                        Acidez:
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Leve, cítrica prazerosa
+
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table4} cellspacing="">
+                            <tr>
+                                <th className={styles.larguraMenor2}>
+                                    <div >
+                                        Corpo:
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Extremamente alta
+
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <table className={styles.table4}>
+                            <tr>
+                                <th className={styles.larguraMenor2}>
+                                    <div >
+                                        Licoroso
+                                        <br />
+                                    </div>
+                                </th>
+                                <th>
+                                    <div >
+                                        Extremamente alta
+
+                                    </div>
+                                </th>
+                            </tr>
+                        </table>
+                        <Logo className={styles.logo} />
+                        <div className={styles.qrcode}>
                             {/* Inserir o endpoint da tela da fazenda na URL do QR code*/}
-                            <RequestQrCode />
-                            {/* <img className={styles.qrCodeCoffee} src={localStorage.getItem('qr_code')} alt={""} /> */}
-                        </th>
-                    </tr>
-                </table>
-
-
-
+                            <RequestQrCode endpoint={'link para o endpoint da fazenda aqui'} />
+                        </div>
+                    </table>
+                </div>
             </div>
         );
     }
