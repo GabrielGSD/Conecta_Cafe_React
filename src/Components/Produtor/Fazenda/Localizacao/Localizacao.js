336c6a421fec09116199bf5a8933ee33076c6ca9
MoisesSDelmoro <moisesdelmoro@gec.inatel.br>
2021-11-11 11:18:06 -0300

    Maps buscando localização - obs. apenas com double click

--- src/Components/Produtor/Fazenda/Localizacao/Localizacao.js
+++ src/Components/Produtor/Fazenda/Localizacao/Localizacao.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { observer } from "mobx-react";
 import { Input } from '../../../Form/Input/Input';
 import { Container, Row } from 'react-bootstrap';
@@ -7,15 +7,43 @@ import Maps from './Maps/Maps'
 import { ButtonSalvar } from '../../../Button/Button';
 import useForm from '../../../../Hooks/useForm';
 
+
+const API_GEOLOCATION_GOOGLE = 'https://maps.googleapis.com/maps/api/geocode/json?'
+const REACT_APP_API_KEY = `${process.env.REACT_APP_API_KEY || "API-KEY NOT FOUND!"}`
+
 const Localizacao = observer(props => {
     const endereco = useForm();
+    const [latitude, setLatitude] = useState('-22.2797829')
+    const [longitude, setLongitude] = useState('-46.3722224')
+    const [localizacao, setLocalizacao] = useState('Ouro Fino, MG')
+
+    const URL_DA_REQUISICAO = API_GEOLOCATION_GOOGLE + 'address=' + localizacao + '&key=' + REACT_APP_API_KEY
 
     // Rua: Maria Joaquina, 185, Crisólia, Ouro Fino, MG
-    var localizacao = ''
-    const handleLocalizacao = () => {
-        localizacao = endereco.value
-        console.log(localizacao)
+    const handleLocalizacao = (event) => {
+        event.preventDefault()
+
+        if (endereco.value) {
+            setLocalizacao(endereco.value)
+
+            if (localizacao == endereco.value) {
+                fetch(URL_DA_REQUISICAO).then((response) => response.json())
+                    .then((responseJson) => {
+                        if (responseJson.status === 'OK') {
+                            setLatitude(responseJson.results[0].geometry.location.lat);
+                            setLongitude(responseJson.results[0].geometry.location.lng);
+                        }
+                    })
+
+                console.log(URL_DA_REQUISICAO)
+                console.log(latitude)
+                console.log(longitude)
+            }
+        } else {
+            console.log("Nenhum endereço inserido!!!")
+        }
     }
+
     return (
         <>
             <h1 className={styles.subTitle}>Localização</h1>
@@ -25,7 +53,7 @@ const Localizacao = observer(props => {
                         <Input label="Endereço" type="text" name="endereco" placeholder="Entre com seu endereco (Rua, N°, Bairro, Cidade, UF)" show={false} {...endereco} />
                     </Row>
                     <ButtonSalvar style={{ width: '130px', marginTop: '15px', backgroundColor: '#dddddd', color: '#666666' }} onClick={handleLocalizacao} >Buscar</ButtonSalvar>
-                    <Maps address={localizacao ? localizacao : "ouro fino, mg"} />
+                    <Maps latitude={latitude} longitude={longitude} />
                 </Container>
             </div>
         </>
--- src/Components/Produtor/Fazenda/Localizacao/Maps/Maps.js
+++ src/Components/Produtor/Fazenda/Localizacao/Maps/Maps.js
@@ -3,34 +3,11 @@ import { observer } from 'mobx-react-lite';
 import React, { useEffect, useState } from "react";
 import styles from '../../Fazenda.module.css';
 
-const API_GEOLOCATION_GOOGLE = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
 const REACT_APP_API_KEY = `${process.env.REACT_APP_API_KEY || "API-KEY NOT FOUND!"}`
 
-// Criar uma função para coletar os dados de endereço do backend e manter no padrão abaixo
-// const ADDRESS = 'address=%20rua%20maria%20joaquina,%20185,%20crisolia,%20mg'
-
 const Maps = observer(props => {
-    const { address } = props
-    const URL_DA_REQUISICAO = API_GEOLOCATION_GOOGLE + 'address=' + address + '&key=' + REACT_APP_API_KEY
-    console.log(URL_DA_REQUISICAO)
-
-    const [lat, setLat] = useState('-22.2797829')
-    const [long, setLong] = useState('-46.3722224')
-
-    useEffect(() => {
-        fetch(URL_DA_REQUISICAO).then((response) => response.json())
-            .then((responseJson) => {
-                if (responseJson.status === 'OK') {
-                    setLat(responseJson.results[0].geometry.location.lat);
-                    setLong(responseJson.results[0].geometry.location.lng);
-                }
-            })
-    }, [address])
-
-    useEffect(() => {
-        console.log(lat)
-        console.log(long)
-    }, [lat, long])
+    const { latitude, longitude } = props
 
     const containerStyle = {
         width: '25%',
@@ -42,17 +19,17 @@ const Maps = observer(props => {
             <div className={styles.containerMaps}>
                 <Map
                     google={window.google}
-                    initialCenter={{
-                        lat: lat,
-                        lng: long
+                    center={{
+                        lat: latitude,
+                        lng: longitude
                     }}
                     zoom={16}
                     containerStyle={containerStyle} >
                     <Marker
                         name={'NomeFazenda'} // Nome que virá do Backend
                         position={{
-                            lat: lat,
-                            lng: long
+                            lat: latitude,
+                            lng: longitude
                         }}
                     />
                 </Map>
