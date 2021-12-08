2ffe17098dfe18960b89332d15f2e6159fbca679
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2021-12-07 22:45:10 -0300

    Finalizado menu mobile

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,52 +1,106 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 import { ReactComponent as Logo } from '../../Assets/logo_white.svg';
 import { UserContext } from '../../Context/UserContext';
+import useMedia from '../../Hooks/useMedia';
 
 function Header() {
-
+  
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = React.useState(false);
   const { login, userLogout } = React.useContext(UserContext);
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
 
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav}`}>
         <Logo />
-        
-        <p style={{ color: '#f31', margin: '1rem 0' }}> {login} </p>
-        <div className={styles.container}>
-          <Link to="/">
-            Home
-          </Link>
-          <Link to="/fazendas">
-            {login ? ( "Outras Fazendas" ) : ("Fazendas")}
-          </Link>
 
-          {login ? (
-            <>
-              <Link to="/conta/fazenda" style={{borderLeft: ' 1.5px solid #fff', paddingLeft: '25px'}}>
-                Fazenda
+        {mobile && (
+          <button
+            aria-label="Menu"
+            className={`${styles.mobileButton} ${
+              mobileMenu && styles.mobileButtonActive
+            }`}
+            onClick={() => setMobileMenu(!mobileMenu)}
+          ></button>
+        )}
+
+        {mobileMenu && mobile ? (
+            <div className={styles.mobileMenu}>
+              <Link to="/">
+                Home
               </Link>
-              <Link to="/conta/cafe">
-                Café
+              <Link to="/fazendas">
+                {login ? ( "Outras Fazendas" ) : ("Fazendas")}
               </Link>
-              <div className={styles.auth} style={{paddingLeft: '15px'}}>
-                <Link to="/login" onClick={userLogout}>
-                  SAIR
-                </Link>
-              </div>
-            </>
+
+              {login ? (
+                <>
+                  <Link to="/conta/fazenda" style={{borderLeft: ' 1.5px solid #fff', paddingLeft: '25px'}}>
+                    Fazenda
+                  </Link>
+                  <Link to="/conta/cafe">
+                    Café
+                  </Link>
+                  <div className={styles.auth} style={{paddingLeft: '15px'}}>
+                    <Link to="/login" onClick={userLogout}>
+                      SAIR
+                    </Link>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">
+                    Entrar
+                  </Link>
+                  <Link to="/cadastro">
+                    Cadastrar
+                  </Link>
+                </>
+              )}
+            </div>
           ) : (
-            <div className={styles.auth}>
-              <Link to="/login">
-                Entrar
+            <div className={styles.container}>
+              <Link to="/">
+                Home
               </Link>
-              <Link to="/cadastro">
-                Cadastrar
+              <Link to="/fazendas">
+                {login ? ( "Outras Fazendas" ) : ("Fazendas")}
               </Link>
+
+              {login ? (
+                <>
+                  <Link to="/conta/fazenda" style={{borderLeft: ' 1.5px solid #fff', paddingLeft: '25px'}}>
+                    Fazenda
+                  </Link>
+                  <Link to="/conta/cafe">
+                    Café
+                  </Link>
+                  <div className={styles.auth} style={{paddingLeft: '15px'}}>
+                    <Link to="/login" onClick={userLogout}>
+                      SAIR
+                    </Link>
+                  </div>
+                </>
+              ) : (
+                <div className={styles.auth}>
+                  <Link to="/login">
+                    Entrar
+                  </Link>
+                  <Link to="/cadastro">
+                    Cadastrar
+                  </Link>
+                </div>
+              )}
             </div>
           )}
-        </div>
       </nav>
     </header>
   );
